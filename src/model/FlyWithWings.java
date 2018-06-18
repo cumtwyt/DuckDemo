dd1a92dc1e9c2c2779d7580fb5a5814196051d69
new <new@DESKTOP-13UJCNG>
2018-06-18 14:22:03 +0800

    DuckDemo

---
+++ src/model/FlyWithWings.java
@@ -0,0 +1,11 @@
+package model;
+
+public class FlyWithWings implements FlyBehavior {
+
+	@Override
+	public void performFly() {
+		// TODO Auto-generated method stub
+		System.out.println("This duck could fly with wings!");
+	}
+
+}
---
+++ src/model/MuteQuack.java
@@ -0,0 +1,11 @@
+package model;
+
+public class MuteQuack implements QuackBehavior {
+
+	@Override
+	public void performQuack() {
+		// TODO Auto-generated method stub
+		System.out.println("This duck could not quack!");
+	}
+
+}
